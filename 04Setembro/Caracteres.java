fc284cb9d78f7095f1365399a0210e33982a1489
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-04 14:46:34 -0300

    Update Caracteres.java

--- 04Setembro/Caracteres.java
+++ 04Setembro/Caracteres.java
@@ -6,5 +6,26 @@ import java.util.Scanner;
 public class Caracteres {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+
+        int vogais = 0;
+        int contador = 0;
+        int resto = 0;
+
+        for (int i = 0; ;i++) {
+            System.out.println("Digite os carácteres: ");
+            char car = teclado.next().charAt(0);
+            if (car == '.')
+                break;
+            if (car == 'a' || car == 'e' || car == 'i' || car == 'o' ||car == 'u') {
+                vogais++;
+            } else {
+                resto++;
+            }
+            contador++;
+        }
+
+        System.out.println("A quantidade total de digitos foi: " + contador);
+        System.out.println("A quantidade total de vogais foi: " + vogais);
+        System.out.println("A quantidade dos demais caracteres foi: " + resto);
     }
 }
