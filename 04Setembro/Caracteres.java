2da85972169e41086e02fdfcf7d3c40fe69193d7
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-04 14:54:21 -0300

    Update Caracteres.java

--- 04Setembro/Caracteres.java
+++ 04Setembro/Caracteres.java
@@ -8,23 +8,25 @@ public class Caracteres {
         Scanner teclado = new Scanner(System.in);
 
         int vogais = 0;
-        int contador = 0;
+        int digitos = 0;
         int resto = 0;
 
-        for (int i = 0; ;i++) {
+        // Lembrando que no loop for, você não precisa dar nenhuma condição inicial
+        for (;;) {
             System.out.println("Digite os carácteres: ");
             char car = teclado.next().charAt(0);
             if (car == '.')
                 break;
             if (car == 'a' || car == 'e' || car == 'i' || car == 'o' ||car == 'u') {
                 vogais++;
+            } else if (car >= '0' && car <= '9') {
+                digitos++;
             } else {
                 resto++;
             }
-            contador++;
         }
 
-        System.out.println("A quantidade total de digitos foi: " + contador);
+        System.out.println("A quantidade total de digitos foi: " + digitos);
         System.out.println("A quantidade total de vogais foi: " + vogais);
         System.out.println("A quantidade dos demais caracteres foi: " + resto);
     }
