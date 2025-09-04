f15aa551700339d9e9f7683503c923784892b801
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-04 14:14:20 -0300

    Update Alunos.java

--- 04Setembro/Alunos.java
+++ 04Setembro/Alunos.java
@@ -1,6 +1,5 @@
 /*
-Ler um número de alunos n. Em seguida, ler as notas dos n alunos e
-imprimir, ao final, a média da turma.
+Ler um número de alunos n. Em seguida, ler as notas dos n alunos e imprimir, ao final, a média da turma.
  */
 import java.util.Scanner;
 
@@ -8,5 +7,19 @@ public class Alunos {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
+        System.out.println("Informe o número de alunos: ");
+        int n = teclado.nextInt();
+
+        double soma = 0;
+        double contador = 0;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Informe a nota do aluno " + i + ":");
+            double nota = teclado.nextDouble();
+            soma += nota;
+            contador++;
+        }
+
+        System.out.println("A média da turma é: " + (soma / contador));
     }
 }
