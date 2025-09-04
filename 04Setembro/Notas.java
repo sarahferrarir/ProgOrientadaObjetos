9aeb85484548fde8082e748d551af0ce6dd1fe72
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-04 12:36:48 -0300

    Create Notas.java

---
+++ 04Setembro/Notas.java
@@ -0,0 +1,12 @@
+/*
+Ler notas de alunos até que o usuário digite -1.
+Ao final imprimir a quantidade de alunos, a média da turma, a maior nota e a menor nota.
+ */
+import java.util.Scanner;
+
+public class Notas {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+    }
+}
