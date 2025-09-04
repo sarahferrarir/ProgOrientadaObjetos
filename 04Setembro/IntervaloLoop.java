245d0fd46b604590faa8a499805d6989d827e605
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-04 12:37:59 -0300

    Create IntervaloLoop.java

---
+++ 04Setembro/IntervaloLoop.java
@@ -0,0 +1,11 @@
+/*
+Ler dois números inteiros (a e b) e imprimir os pares no intervalo a..b, além da soma e da média desses números.
+Caso a seja maior que b, imprima os números no intervalo b..a.
+ */
+import java.util.Scanner;
+
+public class IntervaloLoop {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+    }
+}
