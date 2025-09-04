426ce4abc8d1170152d0b3f598f838afb0e04830
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-04 12:36:25 -0300

    Create Caracteres.java

---
+++ 04Setembro/Caracteres.java
@@ -0,0 +1,12 @@
+/*
+Ler caracteres até que o usuário digite '.' (ponto). Ao final imprimir: a
+quantidade de vogais, a quantidade de dígitos e a quantidade dos demais
+caracteres.
+ */
+import java.util.Scanner;
+
+public class Caracteres {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+    }
+}
