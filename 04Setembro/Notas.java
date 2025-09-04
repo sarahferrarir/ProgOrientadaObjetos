66cb32a7ea0d01242a3b80ffbe8e5ed8f1a02c2a
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-04 14:30:48 -0300

    Update Notas.java

--- 04Setembro/Notas.java
+++ 04Setembro/Notas.java
@@ -8,5 +8,28 @@ public class Notas {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
+        double soma = 0;
+        double contador = 0;
+        double maior = 0;
+        double menor = 10;
+
+        for (int i = 1; ;i++) {
+            System.out.println("Informe a nota do aluno " + i + ":");
+            double nota = teclado.nextDouble();
+            if (nota == -1)
+                break;
+            if (nota > maior){
+                maior = nota;
+            } else if (nota < menor) {
+                menor = nota;
+            }
+            soma += nota;
+            contador++;
+        }
+
+        System.out.println("A turma possui " + contador + " alunos.");
+        System.out.println("A média da turma é: " + (soma / contador));
+        System.out.println("A maior nota da turma foi: " + maior);
+        System.out.println("A menor nota da turma foi: " + menor);
     }
 }
