8f05b64751985f33b504041b68c351363006b3f4
Sarah Ferrari <sarahferrarir@gmail.com>
2025-09-04 12:37:09 -0300

    Create Alunos.java

---
+++ 04Setembro/Alunos.java
@@ -0,0 +1,12 @@
+/*
+Ler um número de alunos n. Em seguida, ler as notas dos n alunos e
+imprimir, ao final, a média da turma.
+ */
+import java.util.Scanner;
+
+public class Alunos {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+    }
+}
